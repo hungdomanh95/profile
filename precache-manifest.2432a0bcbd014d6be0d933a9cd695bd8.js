7a277edc2198528fb2831900d20e47d906ca4c8a
hungdomanh95 <hungdomanh95@gmail.com>
2020-06-01 14:18:02 +0700

    Updates

---
+++ precache-manifest.2432a0bcbd014d6be0d933a9cd695bd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ee73e0ae774f1364b1359b8239b066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42ca6b4df2f982148035",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0bee5c9785fd8f865b2f",
+    "url": "/static/js/2.3dd43b9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dd43b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ca6b4df2f982148035",
+    "url": "/static/js/main.f0db1f5f.chunk.js"
+  },
+  {
+    "revision": "20f3b6762a297931f6d4",
+    "url": "/static/js/runtime-main.d85bd72a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
